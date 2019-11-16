ca8b8200f1fe50afe381646da5c47f70f6f7cb67
Nathaniel <nathanielmorales01@gmail.com>
2019-11-16 15:59:17 +1100

    initial server

---
+++ server.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+// Index Router
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connected To Mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`You are running on Port ${process.env.PORT || 3000}`);
+});
